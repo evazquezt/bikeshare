ff97d1fffbc03458c9189cb034fdaf2eeae371ad
John Sears <johnstuartsears@gmail.com>
2014-03-01 17:48:52 -0800

    We have a package!

--- BikeshareData/R/City.R
+++ BikeshareData/R/City.R
@@ -1,9 +1,9 @@
-City <- setClass("City", slots = c(name="character",
+BikeshareCity <- setClass("BikeshareCity", slots = c(name="character",
                              delim="character",
                              header="logical",
                              keepCols="numeric",
                              memberTypes="character",
                              timeFormat = "character"))
 
-WAS = City(name="Washington, DC", delim=",", header=TRUE, keepCols = c(2,3,4,5,6,7), timeFormat="%m/%d/%Y %H:%M", memberTypes=c("Registered","Casual"))
+WAS = BikeshareCity(name="Washington, DC", delim=",", header=TRUE, keepCols = c(2,3,4,5,6,7), timeFormat="%m/%d/%Y %H:%M", memberTypes=c("Registered","Casual"))
 
