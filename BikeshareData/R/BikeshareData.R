38e30a5bcc9ef1bd3f1b81b8d1993156661a8ea5
John Sears <johnstuartsears@gmail.com>
2014-03-01 18:28:02 -0800

    Added as.data.frame and validity methods to BikeshareData

--- BikeshareData/R/BikeshareData.R
+++ BikeshareData/R/BikeshareData.R
@@ -8,37 +8,19 @@ BikeshareData <- setClass("BikeshareData",
              memberType = "integer",
              stationNames = "character"))
 
-path = "data/was/2010-4th-quarter.csv"
-readTripData <-  function(path, city=WAS){
-    # Allow user to enter String format for city
-    if(class(city) == "character"){
-        city = get(str)
-        if(class(city) != "City"){
-            stop("City must be a valid Bikeshare city")
-        }
-    }
-    
-    data = read.table(path, sep=city@delim, header=city@header, stringsAsFactors=FALSE, quote="\"")
+as.data.frame.BikeshareData = function(from){data.frame(from@startTime,from@endTime, from@startLoc, from@endLoc, from@bike, from@memberType)}
 
-    # Rip out columns that we want
-    data = data[, city@keepCols]
-    colnames(data) = c("startTime","endTime","startLoc","endLoc", "bike","memberType")
-    # Convert dates/times to POSIX
-    startTime = as.POSIXlt(data$startTime, format=city@timeFormat)
-    endTime   = as.POSIXlt(data$endTime, format=city@timeFormat)
-
-    # Pull station data
-    stationNames = union(unique(data$startLoc), unique(data$endLoc))
-
-    startLoc = sapply(data$startLoc, FUN = function(x){which(stationNames == x)}, USE.NAMES=FALSE)
-    endLoc = sapply(data$endLoc, FUN = function(x){which(stationNames == x)}, USE.NAMES=FALSE)
-
-    # Pull bike data
-    bikeNames = unique(data$bike)
-    bike = sapply(data$bike, FUN = function(x){which(bikeNames == x)}, USE.NAMES=FALSE)
-
-    # Member types
-    memberType = sapply(data$memberType, FUN=function(x){which(city@memberTypes == x)}, USE.NAMES=FALSE)
-    bd = BikeshareData(location = city@name, startTime=startTime, endTime=endTime, startLoc = startLoc, endLoc = endLoc, bike=bike, memberType=memberType, stationNames = stationNames)
-    bd
-}
+setValidity("BikeshareData",
+            function(object) {
+                messages <- character()
+                slots <- c("startTime", "endTime", "startLoc", "endLoc","bike","memberType")
+                lengths <- sapply(slots,
+                   function(what) length(slot(object, what)))
+                if(length(unique(lengths))>1)
+                    messages <- paste("unequal lengths: ",
+                                      paste(slots, lengths, sep =":", collapse = ", "))
+                 if(length(messages))
+                    messages
+                else
+                    TRUE
+            })
