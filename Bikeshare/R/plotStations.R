ed9f93ea9d699158ce0125a593d013f8bf234ee1
skwas <sara.kwasnick@gmail.com>
2014-03-14 00:06:37 -0700

    Rebuilt package and updated Rd files for plotStations and getBikingDistance

--- Bikeshare/R/plotStations.R
+++ Bikeshare/R/plotStations.R
@@ -1,8 +1,6 @@
-## Function to plot station location on a map
+## Function to plot station locations on a map
 
-####### need to update Rd file
-
-plotStations <- function(station.data.object,sbst=TRUE,zoom=13,col="red"){
+plotStations <- function(station.data.object,sbst=TRUE,zoom=13,col="red",api_key){
 	
 	## convert data object to a dataframe
 	stations.df <- subset(makeStationDataFrame(station.data.object),sbst)
@@ -11,8 +9,8 @@ plotStations <- function(station.data.object,sbst=TRUE,zoom=13,col="red"){
 	center <- c(mean(locations$long),mean(locations$lat))
 	
 	## plot station locations
-	map <- get_map(location=center,zoom=zoom,source="google")
-	mapbg <- ggmap(map,fullpage=TRUE)
+	map <- get_map(location=center,zoom=zoom,scale = "auto",source="google",api_key)
+	mapbg <- ggmap(map)
 	
 	mapbg + geom_point(data = locations, aes(x = long, y = lat), colour = col, size = 2)
 }
