6648b0c5d9d41008b78d432b8ff5071d1a025697
John Sears <johnstuartsears@gmail.com>
2014-03-13 23:34:41 -0700

    Added Chicago, made cities a hidden function, made station removal date come in as NA if not present

--- Bikeshare/R/City.R
+++ Bikeshare/R/City.R
@@ -5,5 +5,10 @@ BikeshareCity <- setClass("BikeshareCity", slots = c(name="character",
                              memberTypes="character",
                              timeFormat = "character"))
 
-WAS = BikeshareCity(name="Washington, DC", delim=",", header=TRUE, keepCols = c(2,3,4,5,6,7), timeFormat="%m/%d/%Y %H:%M", memberTypes=c("Registered","Casual"))
+.cities <- function(){
+    WAS = BikeshareCity(name="Washington, DC", delim=",", header=TRUE, keepCols = c(2,3,4,5,6,7), timeFormat="%m/%d/%Y %H:%M", memberTypes=c("Registered","Casual"))
+    CHI = BikeshareCity(name="Chicago", delim=",", header=TRUE, keepCols=c(2,3,6,8,4,10), timeFormat="%Y-%m-%d %H:%M", memberTypes=c("Subscriber","Customer"))
+    
+    list(WAS= WAS, CHI=CHI)
+}
 
