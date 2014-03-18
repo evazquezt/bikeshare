a5ee14bd981a652291993afa2f85ae240c6a44d5
John Sears <johnstuartsears@gmail.com>
2014-03-18 16:29:11 -0700

    A few edits to vignette, formatting switch

--- Bikeshare/R/getTotalVisits.R
+++ Bikeshare/R/getTotalVisits.R
@@ -5,13 +5,13 @@ getTotalVisits <- function(bikeshareData,tripSubset=TRUE,stationSubset=TRUE,incl
   
   ## If includeSelf==FALSE, drop trips from a station to itself
   if(includeSelf==FALSE){stationPairs <- stationPairs[stationPairs[,1]!=stationPairs[,2]]}
-	
-	## Sum up visits for each station
-	departures <- sqldf("select X1 station, sum(trips) d from stationPairs group by X1")
-	arrivals <- sqldf("select X2 station, sum(trips) a from stationPairs group by X2")
-	totalVisits <- merge(departures,arrivals)
-	totalVisits <- data.frame(station=totalVisits$station,visits=
-	                            as.numeric(as.character(totalVisits$a))+as.numeric(as.character(totalVisits$d)))
   
-	return(totalVisits)
+  ## Sum up visits for each station
+  departures <- sqldf("select X1 station, sum(trips) d from stationPairs group by X1")
+  arrivals <- sqldf("select X2 station, sum(trips) a from stationPairs group by X2")
+  totalVisits <- merge(departures,arrivals)
+  totalVisits <- data.frame(station=totalVisits$station,visits=
+                            as.numeric(as.character(totalVisits$a))+as.numeric(as.character(totalVisits$d)))
+  
+  return(totalVisits)
 }
