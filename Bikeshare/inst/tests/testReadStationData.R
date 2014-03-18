3fc4f55e31d45ace8bbc86f3258f6d12382025dc
John Sears <johnstuartsears@gmail.com>
2014-03-18 00:52:25 -0700

    Better unit testing framework

--- Bikeshare/inst/tests/testReadStationData.R
+++ Bikeshare/inst/tests/testReadStationData.R
@@ -1,4 +1,2 @@
-stations = readStationData(system.file("extData/bikeStations.xml", package="Bikeshare"),.cities()$WAS)
-
-expect_that(length(stations) > 0, is_true())
-expect_that(nrow(makeStationDataFrame(stations)) > 0, is_true())
+expect_true(length(stations) > 0)
+expect_true(nrow(makeStationDataFrame(stations)) > 0)
