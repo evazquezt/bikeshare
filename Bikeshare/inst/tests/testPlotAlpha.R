d3a05e868d78aa31bfe59fc911931f6dc5d5ae56
John Sears <johnstuartsears@gmail.com>
2014-03-18 22:09:56 -0700

    Updated manual to use \dontrun and tests

--- Bikeshare/inst/tests/testPlotAlpha.R
+++ Bikeshare/inst/tests/testPlotAlpha.R
@@ -1,4 +1,4 @@
 stationSubset <- "stationId <= 31010"
 alpha = plotAlpha(bd,stationSubset=stationSubset,zoom=14)
 
-assert_true(sum(class(alpha) %in% c("gg","ggplot")) == 2)
+expect_true(sum(class(alpha) %in% c("gg","ggplot")) == 2)
--- Bikeshare/inst/tests/testPlotBubbles.R
+++ Bikeshare/inst/tests/testPlotBubbles.R
@@ -1,4 +1,4 @@
 stationSubset <- "stationId <= 31020"
-suppressWarnings(bub = plotBubbles(bd,stationSubset=stationSubset,zoom=14,alpha=.5))
+suppressWarnings(bub <- plotBubbles(bd,stationSubset=stationSubset,zoom=14,alpha=.5))
 
 expect_true(sum(class(bub) %in% c("gg","ggplot")) == 2)
